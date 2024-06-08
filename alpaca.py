563e0b118cbf1936e6481033dd517808b63edc1c
Lj1ang <2872509481@qq.com>
2024-06-09 00:12:17 +0800

    update alpaca
    Signed-off-by: Lj1ang <2872509481@qq.com>

--- alpaca.py
+++ alpaca.py
@@ -7,23 +7,22 @@ output_file_path = 'MathInstruct/MathInstruct_alpaca.json'
 # 读取原始JSON文件
 with open(input_file_path, 'r', encoding='utf-8') as input_file:
     data = json.load(input_file)
-
 # 转换数据到新的格式
-alpaca_formatted_data = []
-for item in data:
-    # 提取问题和答案
-    instruction = item['instruction']
-    output = item['output']
-    # 构建新的结构，包括空的"input"
-    alpaca_entry = {
-        "instruction": instruction,
-        "input": "",
-        "output": output
-     }
-alpaca_formatted_data.append(alpaca_entry)
+    alpaca_formatted_data = []
+    for item in data:
+        # 提取问题和答案
+        instruction = item['instruction']
+        output = item['output']
+        # 构建新的结构，包括空的"input"
+        alpaca_entry = {
+            "instruction": instruction,
+            "input": "",
+            "output": output
+         }
+    alpaca_formatted_data.append(alpaca_entry)
 
-# 写入新的JSON文件
-with open(output_file_path, 'w', encoding='utf-8') as output_file:
-    json.dump(alpaca_formatted_data, output_file, ensure_ascii=False, indent=4)
+    # 写入新的JSON文件
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        json.dump(alpaca_formatted_data, output_file, ensure_ascii=False, indent=4)
 
 print(f"Data has been converted to alpaca format and saved to {output_file_path}")
